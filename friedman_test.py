73879134bd3f116717897af34d9c79fb653f6a4a
Jorge Delgado <ajffdelgado@hotmail.com>
2018-11-29 13:27:39 -0300

    Resultados Estastísticos

--- friedman_test.py
+++ friedman_test.py
@@ -8,4 +8,4 @@ Created on Wed Nov 28 22:06:49 2018
 import numpy as np                                                         
 import scipy as sp                                                         
 import scipy.stats as sps
-sol =  sps.friedmanchisquare([79.52,92.06,24],[43.38,54.54,31],[79.43,88.60,46])
+sol =  sps.friedmanchisquare([79.52,92.06,79.59],[43.38,54.54,46.82],[79.43,88.60,79.57])
--- nemenyi_test.py
+++ nemenyi_test.py
@@ -8,5 +8,5 @@ Created on Wed Nov 28 23:54:01 2018
 import scikit_posthocs as sp
 import pandas as pd
 import numpy as np
-x = np.array([[79.52,92.06,24],[43.38,54.54,31],[79.43,88.60,46]])
+x = np.array([[79.52,92.06,79.59],[43.38,54.54,46.82],[79.43,88.60,79.57]])
 sol =  sp.posthoc_nemenyi_friedman(x)
